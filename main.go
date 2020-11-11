5be0b8aa505d6429a20bfa3b091123f62e5f20f2
Divyang <52504061+DIVYANGR@users.noreply.github.com>
2020-11-11 10:45:12 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,19 +10,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Employee struct {
-	Id         int    `json:id`
-	Name       string `json:name`
-	City       string `json:city`
-	Department string `json:department`
-	Salary     int    `json:salary`
+type product struct {
+	Id      int    `json:id`
+	Name    string `json:name`
+	price   string `json:price`
+	quality string `json:quality`
 }
 
 func dbConn() (db *sql.DB) {
 	dbDriver := "mysql"
-	dbUser := "root"
+	dbUser := "divyang"
 	dbPass := "divyangk1998"
-	dbName := "divyang"
+	dbName := "golang"
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	if err != nil {
 		panic(err.Error())
@@ -35,79 +34,100 @@ func main() {
 	router.POST("/add", func(c *gin.Context) {
 
 		name := c.Query("name")
-		city := c.Query("city")
-		department := c.Query("department")
-		salary := c.Query("salary")
+		price := c.Query("price")
+		quality := c.Query("quality")
 
 		c.JSON(200, gin.H{
-			"name":       name,
-			"city":       city,
-			"department": department,
-			"salary":     salary,
+			"name":    name,
+			"price":   price,
+			"quality": quality,
 		})
 		db := dbConn()
-		insForm, err := db.Prepare("INSERT INTO employee(name, city, department, salary) VALUES(?,?,?,?)")
+		insForm, err := db.Prepare("INSERT INTO product(name, price, quality) VALUES(?,?,?)")
 		if err != nil {
 			panic(err.Error())
 		}
-		insForm.Exec(name, city, department, salary)
-		fmt.Printf("name: %s; city: %s; department: %s; salary: %d", name, city, department, salary)
+		insForm.Exec(name, price, quality)
+		fmt.Printf("name: %s; price: %s; quality: %s", name, price, quality)
 	})
 
 	router.PUT("/update", func(c *gin.Context) {
 		id1 := c.Query("id")
 		name := c.Query("name")
-		city := c.Query("city")
-		department := c.Query("department")
-		salary := c.Query("salary")
+		price := c.Query("price")
+		quality := c.Query("quality")
 
 		c.JSON(200, gin.H{
-			"name":       name,
-			"city":       city,
-			"department": department,
-			"salary":     salary,
+			"name":    name,
+			"price":   price,
+			"quality": quality,
 		})
 		db := dbConn()
-		upForm, err := db.Prepare("INSERT INTO employee(id, name, city, department, salary) VALUES(?,?,?,?,?) Where id=id1")
+		upForm, err := db.Prepare("UPDATE product SET name=?, price=?, quality=? Where id=?")
 		if err != nil {
 			panic(err.Error())
 		}
-		upForm.Exec(id1, name, city, department, salary)
-		fmt.Printf("name: %s; city: %s; department: %s; salary: %d", name, city, department, salary)
+		upForm.Exec(name, price, quality, id1)
+		fmt.Printf("name: %s; price: %s; quality: %s", name, price, quality)
 	})
 
 	router.GET("/GET", func(c *gin.Context) {
 		id := c.Query("id")
 		db := dbConn()
-		selDB, err := db.Query("SELECT * FROM employee WHERE id=?", id)
+		selDB, err := db.Query("SELECT * FROM product WHERE id=?", id)
 		if err != nil {
 			panic(err.Error())
 		}
-		var salary int
-		var name, city, department string
+
+		var name, price, quality string
 		for selDB.Next() {
 
-			err = selDB.Scan(&id, &name, &city, &department, &salary)
+			err = selDB.Scan(&id, &name, &price, &quality)
 			if err != nil {
 				panic(err.Error())
 			}
 		}
-		fmt.Printf("name: %s; city: %s; department: %s; salary: %d", name, city, department, salary)
+		fmt.Printf("name: %s; price: %s; quality: %s", name, price, quality)
 
 		c.JSON(200, gin.H{
-			"id":         id,
-			"name":       name,
-			"city":       city,
-			"department": department,
-			"salary":     salary,
+			"id":      id,
+			"name":    name,
+			"price":   price,
+			"quality": quality,
 		})
+
+	})
+
+	router.GET("/getall", func(c *gin.Context) {
+		db := dbConn()
+		selDB, err := db.Query("SELECT * FROM product")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var id, name, price, quality string
+		for selDB.Next() {
+
+			err = selDB.Scan(&id, &name, &price, &quality)
+			c.JSON(200, gin.H{
+				"id":      id,
+				"name":    name,
+				"price":   price,
+				"quality": quality,
+			})
+			fmt.Printf("name: %s; price: %s; quality: %s", name, price, quality)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+		//fmt.Printf("name: %s; price: %s; quality: %s", name, price, quality)
 
 	})
 
 	router.DELETE("/delete", func(c *gin.Context) {
 		name := c.Query("name")
 		db := dbConn()
-		delForm, err := db.Prepare("DELETE FROM employee WHERE name=?")
+		delForm, err := db.Prepare("DELETE FROM product WHERE name=?")
 		if err != nil {
 			panic(err.Error())
 		}
